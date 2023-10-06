33ac41f2474ce0e715cb8a24d0434ea58d279d3b
Hans-J. Schmid <knock@myopendoor.de>
2023-10-06 04:39:57 +0200

    °

---
+++ tut_io/networking_2.rb
@@ -0,0 +1,13 @@
+#---
+# Excerpted from "Programming Ruby 3.2",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit https://pragprog.com/titles/ruby5 for more book information.
+#---
+require "open-uri"
+
+URI.open("https://pragprog.com/titles/ruby5/programming-ruby-3-2-5th-edition/") do |f|
+  puts f.read.scan(/<img class=".*?" src="(.*?)"/m).uniq[0,3]
+end
